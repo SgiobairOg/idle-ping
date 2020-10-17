1f016bb88f17f7b4aca0c741535d3e34507ad191
Jason Wilson <jason.wilson@valtech.com>
2020-10-18 00:17:05 +0200

    feat(): Add Game Configuration
    - adds basic game information
    - adds basic configuration for enhancements

---
+++ src/configuration/index.js
@@ -0,0 +1,21 @@
+export const config = {
+    gameInfo: {
+        title: 'Basic Clicker',
+    },
+    enhancements: [
+        {
+            name: 'Better Clicker',
+            rate: 3,
+            auto: false,
+            unlock: 10,
+            lore: 'A moderately better clicker, three times faster than the standard click.'
+        },
+        {
+            name: 'Basic Auto Clicker',
+            rate: 1,
+            auto: true,
+            unlock: 20,
+            lore: 'A basic auto clicker, adds one click per second.'
+        },
+    ],
+}
