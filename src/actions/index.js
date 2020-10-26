7d45c8f34cfc144dc47e822c307ead941886c870
Jason Wilson <jason.wilson@valtech.com>
2020-10-26 21:13:12 +0100

    feat(): Update Actions
    - add persistState step and handler
    - add setClicks action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,24 @@
+import { idbClear, idbSet } from './../idb';
+
 export const addToClicks = (store, amount) => {
     const clicks = store.state.clicks + amount;
     store.setState({ clicks });
+    persistState(store.state);
+};
+
+export const setClicks = (store, clicks) => {
+    store.setState({ clicks });
+    persistState(store.state);
 };
 
 export const setRate = (store, rate) => {
     store.setState({ rate });
+    persistState(store.state);
 };
 
 export const setAutoRate = (store, autoRate) => {
     store.setState({ autoRate });
+    persistState(store.state);
 };
+
+const persistState = state => idbSet('state', state);
