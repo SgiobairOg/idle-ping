43bd52c789b2b7aaad2bba2c87144796d494c642
Jason Wilson <jason.wilson@valtech.com>
2020-10-26 21:14:30 +0100

    feat(): Integrate Storage and Reset to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 
+import React, { useEffect } from 'react';
+
 import AutoClicker from './components/auto-clicker/AutoClicker';
 import ClickDisplay from './components/click-display/ClickDisplay';
 import Clicker from './components/clicker/Clicker';
 import Enhancement from './components/enhancement/Enhancement';
-import React from 'react';
+import ResetButton from './components/reset-button/ResetButton';
 import { config } from './../src/configuration';
+import { idbGet } from './idb';
+import useGlobal from './store';
 
 function App() {
+  const [, globalActions] = useGlobal();
+
+  useEffect(() => {
+    idbGet('state').then((lastState) => {
+      if( lastState ) {
+        globalActions.addToClicks(lastState.clicks);
+        globalActions.setRate(lastState.rate);
+        globalActions.setAutoRate(lastState.autoRate);
+      }
+    })
+  }, [globalActions])
 
   const enhancements = config.enhancements.map( (enhancement, index) =>
     <Enhancement options={enhancement} key={index}></Enhancement>
@@ -21,6 +36,7 @@ function App() {
       <AutoClicker></AutoClicker>
       <hr></hr>
       {enhancements}
+      <ResetButton>Reset the game</ResetButton>
     </div>
   );
 }
